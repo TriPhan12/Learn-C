97dda62416cb002976d345376ef7258d4647b492
Tri Phan <pmtri96@gmail.com>
2019-02-20 11:40:20 +0700

    switch...case...

--- ex10.c
+++ ex10.c
@@ -1,1 +1,36 @@
+//switch statement
+
 #include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int i = 0;
+    for (i = 1; i < argc; i++){
+        printf ("argument %d: %s \n", i, argv[i] );
+    };
+    char *states[] = {
+        "Cali", "Origon", "Washington", "Texas"
+    };
+
+    int num_state = 4;
+
+    for (i = 0; i < num_state; i++){
+        switch (i)
+        {
+            case 0:
+                printf("The first state is: %s \n", states[i]);
+                break;
+            
+            case 1:
+                printf("The second state is: %s \n",states[i]);                
+                break;
+
+            case 2:
+                printf("The third state is: %s \n", states[i]);
+                break;
+            
+            default:
+                printf("The fourth state is: %s \n",states[i]);                
+                break;
+        }
+    }
+}
